2b91c0b7fd8ff2463646eceecd125d19758c7884
Nicholas Flynt <thegamefreak0134@gmail.com>
2018-07-08 01:10:21 -0500

    Move website files into public directory, for simpler hosting

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+mkdir -p ./public
 cargo +nightly build --target wasm32-unknown-unknown --release
-wasm-bindgen target/wasm32-unknown-unknown/release/rusticnes_wasm.wasm --out-dir . --no-modules
+wasm-bindgen target/wasm32-unknown-unknown/release/rusticnes_wasm.wasm --out-dir ./public --no-modules
