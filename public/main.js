ed3018aa167bc3ac0b1858cdca81038db83c052c
Nicholas Flynt <thegamefreak0134@gmail.com>
2021-11-07 15:21:38 -0500

    Remove the old index/main. The web workers build is now the default

--- public/main.js
+++ public/main.js
@@ -1,64 +1,240 @@
-// Note that for webassembly.studio we're using the CLI's `--no-modules` option
-// which may not be what you're using locally (aka using ES modules/Webpack).
-// Since we're using `--no-modules` the `wasm_bindgen` global is initialized 
-// to the exports of our module, and then below initialization is slightly
-// different.
-//
-// When using wasm-bindgen locally and a different bundler like Webpack (or 
-// a different option like `--nodejs`) these integration points will be
-// tweaked slightly, and you can consult https://github.com/rustwasm/wasm-bindgen
-// for more information.
+// ========== Global Application State ==========
 
-const { 
-  load_rom, 
-  run_until_vblank,   
-  set_p1_input, 
-  set_p2_input, 
-  set_audio_samplerate,
-  set_audio_buffersize,
-  audio_buffer_full, 
-  get_audio_buffer, 
-  get_sram, set_sram, 
-  has_sram, update_windows, 
-  draw_apu_window, 
-  draw_piano_roll_window,
-  draw_screen_pixels,
-  apu_window_click
-} = wasm_bindgen;
+let g_pending_frames = 0;
+let g_frames_since_last_fps_count = 0;
+let g_rendered_frames = [];
 
-var audio_buffer_size = 4096;
-var audio_sample_rate = 44100;
-var last_sample = 0;
-var nes_audio_buffer = [];
-var audio_context;
-try {
-  window.AudioContext = window.AudioContext || window.webkitAudioContext;
-  audio_context = new AudioContext();
-  console.log("Using samplerate: ", audio_context.sampleRate);
-  audio_sample_rate = audio_context.sampleRate;
-} catch(e) {
-  console.log('Web Audio API is not supported in this browser');
+let g_last_frame_sample_count = 44100 / 60; // Close-ish enough
+let g_audio_samples_buffered = 0;
+
+let g_game_checksum = -1;
+
+
+// ========== Worker Setup and Utility ==========
+
+var worker = new Worker('emu_worker.js');
+
+function rpc(task, args) {
+  return new Promise((resolve, reject) => {
+    const channel = new MessageChannel();
+    channel.port1.onmessage = ({data}) => {
+      if (data.error) {
+        reject(data.error);
+      } else {
+        resolve(data.result);
+      }
+    };
+    worker.postMessage({"type": "rpc", "func": task, "args": args}, [channel.port2]);
+  });
 }
 
-var script_processor = audio_context.createScriptProcessor(audio_buffer_size, 1, 1);
-script_processor.onaudioprocess = function(e) {
-  var output = e.outputBuffer.getChannelData(0);
-  if (nes_audio_buffer.length >= audio_buffer_size) {
-    for (var i = 0; i < audio_buffer_size; i++) {
-      // Copy over PCM samples.
-      output[i] = nes_audio_buffer[i];
+worker.onmessage = function(e) {
+  if (e.data.type == "init") {
+    onready();
+  }
+  if (e.data.type == "deliverFrame") {
+    g_rendered_frames.push(e.data.panels);
+    g_pending_frames -= 1;
+    g_frames_since_last_fps_count += 1;
+    if (g_audio_samples_buffered < 8192) {
+      g_nes_audio_node.port.postMessage({"type": "samples", "samples": e.data.audio_buffer});
+      g_audio_samples_buffered += e.data.audio_buffer.length;
+      g_last_frame_sample_count = e.data.audio_buffer.length;
+    } else {
+      // Audio overrun, we're running too fast! Drop these samples on the floor and bail.
+      // (This can happen in fastforward mode.)
     }
-    local_audio_buffer_full = false;
-    last_sample = nes_audio_buffer[audio_buffer_size - 1];
-    nes_audio_buffer = nes_audio_buffer.slice(audio_buffer_size);
-  } else {
-    for (var i = 0; i < audio_buffer_size; i++) {
-      // Underrun: Copy over the very last sample
-      output[i] = last_sample; 
+    if (g_rendered_frames.length > 3) {
+      // Frame rendering running behing, dropping one frame
+      g_rendered_frames.shift(); // and throw it away
     }
   }
 }
-script_processor.connect(audio_context.destination);
+
+// ========== Audio Setup ==========
+
+let g_audio_context = null;
+let g_nes_audio_node = null;
+
+async function init_audio_context() {
+  g_audio_context = new AudioContext({
+    latencyHint: 'interactive',
+    sampleRate: 44100,
+  });
+  await g_audio_context.audioWorklet.addModule('audio_processor.js');
+  g_nes_audio_node = new AudioWorkletNode(g_audio_context, 'nes-audio-processor');
+  g_nes_audio_node.connect(g_audio_context.destination);
+  g_nes_audio_node.port.onmessage = handle_audio_message;
+}
+
+function handle_audio_message(e) {
+  if (e.data.type == "samplesPlayed") {
+    g_audio_samples_buffered -= e.data.count;
+  }
+}
+
+// ========== Main ==========
+
+async function onready() {
+  // Initialize audio context, this will also begin audio playback
+  await init_audio_context();
+
+  // Initialize everything else
+  init_ui_events();
+  initializeButtonMappings();
+
+  // Kick off the events that will drive emulation
+  requestAnimationFrame(renderLoop);
+  // run the scheduler as often as we can. It will frequently decide not to schedule things, this is fine.
+  //window.setInterval(schedule_frames_at_top_speed, 1);
+  window.setTimeout(sync_to_audio, 1);
+  window.setInterval(compute_fps, 1000);
+  window.setInterval(save_sram_periodically, 10000);
+
+  // Attempt to load a cartridge by URL, if one is provided
+  let params = new URLSearchParams(location.search.slice(1));
+  if (params.get("cartridge")) {
+    load_cartridge_by_url(params.get("cartridge"));
+    display_banner(params.get("cartridge"));
+  }
+  if (params.get("tab")) {
+    switchToTab(params.get("tab"));
+  }
+}
+
+function init_ui_events() {
+  // Setup UI events
+  document.getElementById('file-loader').addEventListener('change', load_cartridge_by_file, false);
+
+  var buttons = document.querySelectorAll("#main_menu button");
+  buttons.forEach(function(button) {
+    button.addEventListener("click", clickTab);
+  });
+
+  window.addEventListener("click", function() {
+    // Needed to play audio in certain browsers, notably Chrome, which restricts playback until user action.
+    g_audio_context.resume();
+  });
+
+  document.querySelector("#playfield").addEventListener("dblclick", enterFullscreen);
+  document.addEventListener("fullscreenchange", handleFullscreenSwitch);
+  document.addEventListener("webkitfullscreenchange", handleFullscreenSwitch);
+  document.addEventListener("mozfullscreenchange", handleFullscreenSwitch);
+  document.addEventListener("MSFullscreenChange", handleFullscreenSwitch);
+
+  if (document.querySelector("#apu_window")) {
+    document.querySelector("#apu_window").addEventListener("click", handle_apu_window_click);
+  }
+}
+
+// ========== Cartridge Management ==========
+
+async function load_cartridge(cart_data) {
+  console.log("Attempting to load cart with length: ", cart_data.length);
+  await rpc("load_cartridge", [cart_data]);
+  console.log("Cart data loaded?");
+  
+  g_game_checksum = crc32(cart_data);
+  load_sram();
+  let power_light = document.querySelector("#power_light #led");
+  power_light.classList.add("powered");
+}
+
+function load_cartridge_by_file(e) {
+  if (g_game_checksum != -1) {
+    save_sram();
+  }
+  var file = e.target.files[0];
+  if (!file) {
+    return;
+  }
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    cart_data = new Uint8Array(e.target.result);
+    load_cartridge(cart_data);
+    hide_banners();
+  }
+  reader.readAsArrayBuffer(file);
+
+  // we're done with the file loader; unfocus it, so keystrokes are captured
+  // by the game instead
+  this.blur();
+}
+
+function load_cartridge_by_url(url) {
+  if (g_game_checksum != -1) {
+    save_sram();
+  }
+  var rawFile = new XMLHttpRequest();
+  rawFile.overrideMimeType("application/octet-stream");
+  rawFile.open("GET", url, true);
+  rawFile.responseType = "arraybuffer";
+  rawFile.onreadystatechange = function() {
+    if (rawFile.readyState === 4 && rawFile.status == "200") {
+      console.log(rawFile.responseType);
+      cart_data = new Uint8Array(rawFile.response);
+      load_cartridge(cart_data);
+    }
+  }
+  rawFile.send(null);
+}
+
+// ========== Emulator Runtime ==========
+
+function schedule_frames_at_top_speed() {
+  if (g_pending_frames < 10) {
+    requestFrame();
+  }
+  window.setTimeout(schedule_frames_at_top_speed, 1);
+}
+
+function sync_to_audio() {
+  // Never, for any reason, request more than 10 frames at a time. This prevents
+  // the message queue from getting flooded if the emulator can't keep up.
+  if (g_pending_frames < 10) {
+    const actual_samples = g_audio_samples_buffered;
+    const pending_samples = g_pending_frames * g_last_frame_sample_count;
+    const sample_threshold = 2048;
+    if (actual_samples + pending_samples < sample_threshold) {
+      requestFrame();
+    }
+  }
+  window.setTimeout(sync_to_audio, 1);
+}
+
+function requestFrame() {  
+  let active_tab = document.querySelector(".tab_content.active").id;
+  if (active_tab == "jam") {
+    worker.postMessage({"type": "requestFrame", "p1": keys[1], "p2": keys[2], "panels": [
+      {"id": "screen", "target_element": "#jam_pixels"},
+      {"id": "apu_window", "target_element": "#apu_window"},
+      {"id": "piano_roll_window", "target_element": "#piano_roll_window"},
+    ]});
+  } else {
+    worker.postMessage({"type": "requestFrame", "p1": keys[1], "p2": keys[2], "panels": [
+      {"id": "screen", "target_element": "#pixels"}
+    ]});
+  }
+  g_pending_frames += 1;
+}
+
+function renderLoop() {
+  if (g_rendered_frames.length > 0) {
+    for (let panel of g_rendered_frames.shift()) {
+      const typed_pixels = new Uint8ClampedArray(panel.image_buffer);
+      // TODO: don't hard-code the panel size here
+      let rendered_frame = new ImageData(typed_pixels, panel.width, panel.height);
+      canvas = document.querySelector(panel.target_element);
+      ctx = canvas.getContext("2d", { alpha: false });
+      ctx.putImageData(rendered_frame, 0, 0);
+      ctx.imageSmoothingEnabled = false;
+    }
+  }
+
+  requestAnimationFrame(renderLoop);
+}
+
+// ========== SRAM Management ==========
 
 // CRC32 checksum generating functions, yanked from this handy stackoverflow post and modified to work with arrays:
 // https://stackoverflow.com/questions/18638900/javascript-crc32
@@ -87,16 +263,14 @@ var crc32 = function(byte_array) {
     return (crc ^ (-1)) >>> 0;
 };
 
-var game_checksum = -1;
-
-function load_sram() {
-  if (has_sram()) {
+async function load_sram() {
+  if (await rpc("has_sram")) {
     try {
-      var sram_str = window.localStorage.getItem(game_checksum);
+      var sram_str = window.localStorage.getItem(g_game_checksum);
       if (sram_str) {
         var sram = JSON.parse(sram_str);
-        set_sram(sram);        
-        console.log("SRAM Loaded!", game_checksum);
+        await rpc("set_sram", [sram]);
+        console.log("SRAM Loaded!", g_game_checksum);
       }
     } catch(e) {
       console.log("Local Storage is probably unavailable! SRAM saving and loading will not work.");
@@ -104,153 +278,34 @@ function load_sram() {
   }
 }
 
-function save_sram() {
-  if (has_sram()) {
+async function save_sram() {
+  if (await rpc("has_sram")) {
     try {
-      var sram_uint8 = get_sram();
+      var sram_uint8 = await rpc("get_sram", [sram]);
       // Make it a normal array
       var sram = [];
       for (var i = 0; i < sram_uint8.length; i++) {
         sram[i] = sram_uint8[i];
       }
-      window.localStorage.setItem(game_checksum, JSON.stringify(sram));
-      console.log("SRAM Saved!", game_checksum);
+      window.localStorage.setItem(g_game_checksum, JSON.stringify(sram));
+      console.log("SRAM Saved!", g_game_checksum);
     } catch(e) {
       console.log("Local Storage is probably unavailable! SRAM saving and loading will not work.");
     }
   }
 }
 
-var start_time = 0;
-var current_frame = 0;
-var sram_delay = 600;
-
-var screen_pixels = new Uint8ClampedArray(256*240*4);
-var apu_raw_pixels = new Uint8ClampedArray(256*500*4);
-var piano_roll_raw_pixels = new Uint8ClampedArray(480*270*4);
-
-function draw_window(id, pixel_array, rust_draw_func, width, height) {
-  canvas = document.querySelector(id);
-  rust_draw_func(pixel_array);
-  image_data = new ImageData(pixel_array, width, height);
-  ctx = canvas.getContext("2d", { alpha: false });
-  ctx.putImageData(image_data, 0, 0);
-  ctx.imageSmoothingEnabled = false;
+function save_sram_periodically() {
+  save_sram();
 }
 
-var loaded = false;
+// ========== User Interface ==========
 
-function gameLoop() {
-  if (!loaded) {
-    return;
-  }
-  updateGamepads();
-  set_p1_input(keys[1]);
-  set_p2_input(keys[2]);
-
-  var runtime = Date.now() - start_time;
-  var target_frame = runtime / (1000 / 60);
-  if (target_frame - current_frame > 2) {
-    // We're running behind, reset the timer so we don't run off the deep end
-    start_time = Date.now();
-    current_frame = 0;
-    run_until_vblank();
-    update_windows()
-  } else {
-    while (current_frame < target_frame) {
-      run_until_vblank();
-      update_windows()
-      current_frame += 1;
-    }
-  }
-  if (audio_buffer_full()) {
-    audio_buffer = get_audio_buffer();
-    if (nes_audio_buffer.length < audio_buffer_size * 2) { 
-      for (i = 0; i < audio_buffer_size; i++) {
-        nes_audio_buffer.push(audio_buffer[i] / 32768);
-      }
-    }
-  }
-
-  sram_delay = sram_delay - 1;
-  if (sram_delay <= 0) {
-    save_sram();
-    sram_delay = 600;
-  }
-}
-
-function renderLoop() {
-  // Draw all windows
-  let active_tab = document.querySelector(".tab_content.active").id;
-  if (active_tab == "jam") {
-    draw_window("#jam_pixels", screen_pixels, draw_screen_pixels, 256, 240);
-    draw_window("#apu_window", apu_raw_pixels, draw_apu_window, 256, 500);
-    draw_window("#piano_roll_window", piano_roll_raw_pixels, draw_piano_roll_window, 480, 270);
-  } else {
-    draw_window("#pixels", screen_pixels, draw_screen_pixels, 256, 240);
-  }
-
-  requestAnimationFrame(renderLoop);
-}
-
-window.addEventListener("click", function() {
-  // Needed to play audio in certain browsers, notably Chrome, which restricts playback until user action.
-  audio_context.resume();
-});
-
-function load_cartridge(cart_data) {
-  console.log(cart_data.length);
-  load_rom(cart_data);
-  console.log("Cart data loaded?");
-  set_audio_samplerate(audio_sample_rate);
-  set_audio_buffersize(audio_buffer_size);
-  console.log("Set sample rate to: ", audio_sample_rate);
-  start_time = Date.now();
-  current_frame = 0;
-  game_checksum = crc32(cart_data);
-  load_sram();
-  loaded = true;
-  let power_light = document.querySelector("#power_light #led");
-  power_light.classList.add("powered");
-}
-
-function load_cartridge_by_url(url) {
-  if (game_checksum != -1) {
-    save_sram();
-  }
-  var rawFile = new XMLHttpRequest();
-  rawFile.overrideMimeType("application/octet-stream");
-  rawFile.open("GET", url, true);
-  rawFile.responseType = "arraybuffer";
-  rawFile.onreadystatechange = function() {
-    if (rawFile.readyState === 4 && rawFile.status == "200") {
-      console.log(rawFile.responseType);
-      cart_data = new Uint8Array(rawFile.response);
-      load_cartridge(cart_data);
-    }
-  }
-  rawFile.send(null);
-}
-
-function load_cartridge_by_file(e) {
-  if (game_checksum != -1) {
-    save_sram();
-  }
-  var file = e.target.files[0];
-  if (!file) {
-    return;
-  }
-  var reader = new FileReader();
-  reader.onload = function(e) {
-    cart_data = new Uint8Array(e.target.result);
-    load_cartridge(cart_data);
-    hide_banners();
-  }
-  reader.readAsArrayBuffer(file);
-
-  // we're done with the file loader; unfocus it, so keystrokes are captured
-  // by the game instead
-  this.blur();
+// This runs *around* once per second, ish. It's fine.
+function compute_fps() {
+  let counter_element = document.querySelector("#fps-counter");
+  counter_element.innerText = "FPS: " + g_frames_since_last_fps_count;
+  g_frames_since_last_fps_count = 0;
 }
 
 function clearTabs() {
@@ -341,45 +396,6 @@ function display_banner(cartridge_name) {
   }
 }
 
-function handle_apu_window_click(e) {  
-  apu_window_click(e.offsetX, e.offsetY);
+function handle_apu_window_click(e) {
+  rpc("handle_apu_window_click", [e.offsetX, e.offsetY]).then();
 }
-
-function runApp() {
-  let params = new URLSearchParams(location.search.slice(1));
-  console.log("params", params);
-  if (params.get("cartridge")) {
-    load_cartridge_by_url(params.get("cartridge"));
-    display_banner(params.get("cartridge"));
-  }
-  if (params.get("tab")) {
-    switchToTab(params.get("tab"));
-  }
-  document.getElementById('file-loader').addEventListener('change', load_cartridge_by_file, false);
-  
-  var buttons = document.querySelectorAll("#main_menu button");
-  buttons.forEach(function(button) {
-    button.addEventListener("click", clickTab);
-  });
-
-  initializeButtonMappings();
-
-  document.querySelector("#playfield").addEventListener("dblclick", enterFullscreen);
-  document.addEventListener("fullscreenchange", handleFullscreenSwitch);
-  document.addEventListener("webkitfullscreenchange", handleFullscreenSwitch);
-  document.addEventListener("mozfullscreenchange", handleFullscreenSwitch);
-  document.addEventListener("MSFullscreenChange", handleFullscreenSwitch);
-
-  if (document.querySelector("#apu_window")) {
-    document.querySelector("#apu_window").addEventListener("click", handle_apu_window_click);
-  }
-
-  loadInputConfig();
-  requestAnimationFrame(renderLoop);
-  window.setInterval(gameLoop, 15);
-}
-
-// Load and instantiate the wasm file, and we specify the source of the wasm
-// file here. Once the returned promise is resolved we're ready to go and
-// use our imports.
-wasm_bindgen('./rusticnes_wasm_bg.wasm').then(runApp);
